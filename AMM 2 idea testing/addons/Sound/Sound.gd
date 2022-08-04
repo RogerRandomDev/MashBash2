4ce1937c2d89f96b6c1a4680753a100663ce55ad
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-08-04 15:09:57 -0700

    made the word pull system functional, still need to apply and pull the info from objects

---
+++ AMM 2 idea testing/addons/Sound/Sound.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var sounds=[]
+var music=AudioStreamPlayer.new()
+
+func _ready():
+	add_child(music)
+
+
+
+func play(sound):
+	var player=AudioStreamPlayer.new()
+	add_child(player)
+	sounds.push_back(player)
+	player.stream=load("res://addons/Sound/Sounds/World/%s.wav"%sound)
+	player.connect("finished",removeSound,[player])
+	player.play()
+	
+#plays song
+func playSong(song):
+	music.stream=load("res://addons/Sound/Sounds/Music/%s.mp3"%song)
+	music.play()
+
+
+
+func removeSound(soundPlayer):
+	sounds.erase(soundPlayer)
+	soundPlayer.queue_Free()
--- AMM 2 idea testing/addons/word/wordswapper/wordSwap.gd
+++ AMM 2 idea testing/addons/word/wordswapper/wordSwap.gd
@@ -4,6 +4,8 @@ var Words
 var Owned
 var BaseText=""
 var selectedWord=0
+var activeSet=0
+var activeObject
 func _ready():
 	Owned=$Owned
 	Words=$RichTextLabel
@@ -14,20 +16,40 @@ func _ready():
 
 func _input(_event):
 	var dir=int(Input.is_action_just_pressed("left"))-int(Input.is_action_just_pressed("right"))
-	if Input.is_action_just_pressed("confirm"):
-		if BaseText.split(" ").size()<=1:return
-		var removed=removeWord(selectedWord)
-		if removed!=null:Word.storedWords.push_back(removed)
+	if Input.is_action_just_pressed("up")||Input.is_action_just_pressed("down"):
+		activeSet=1-activeSet
+		if activeSet==1&&Word.storedWords.size()<1:activeSet=0
+		selectedWord=0
 		updateOwned()
+		updateSelectedWord()
+	if Input.is_action_just_pressed("confirm"):
+		if activeSet==0:
+			if BaseText.split(" ").size()<=1:return
+			var removed=removeWord(selectedWord)
+			if removed!=null:Word.storedWords.push_back(removed)
+			
+		if activeSet==1:
+			var added=Word.storedWords[selectedWord]
+			insertWord(added)
+			Word.storedWords.remove_at(selectedWord)
+			if Word.storedWords.size()==0:activeSet=0
+			updateSelectedWord()
+			selectedWord=0
+			
+		updateOwned()
+		
 	if dir==0:return
-	selectedWord=clamp(selectedWord-dir,0,BaseText.split(" ").size()-2)
+	#clamps the chosen words to the size of words you can choose
+	if activeSet==0:selectedWord=clamp(selectedWord-dir,0,BaseText.split(" ").size()-2)
+	if activeSet==1:selectedWord=clamp(selectedWord-dir,0,Word.storedWords.size()-1)
+	
 	updateSelectedWord()
 
 
 #updates the current chosen word
 func updateSelectedWord():
 	var _splitWords=BaseText.split(" ")
-	buildPhrase(_splitWords)
+	Words.text=buildPhrase(_splitWords)
 
 
 #removes currently chosen word and returns it for you
@@ -49,18 +71,34 @@ func buildPhrase(_splitWords,withEffect=true):
 	Words.text=""
 	for i in _splitWords.size():
 		if _splitWords[i]=="":continue
-		if(i==selectedWord&&withEffect&&_splitWords.size()!=1):
+		if(i==selectedWord&&withEffect&&_splitWords.size()!=1&&activeSet==0):
 			Words.text+="[pull]"
 			Words.text+=_splitWords[i]
 			Words.text+="[/pull] "
 			continue
 		Words.text+=_splitWords[i]
 		if i!=_splitWords.size()-1:Words.text+=" "
+	updateOwned()
+	return Words.text
 
 
 #updates the owned words
 func updateOwned():
 	Owned.text=""
-	for word in Word.storedWords:
-		Owned.text+=word+" "
-	Owned.text.trim_suffix(" ")
+	for word in Word.storedWords.size():
+		if selectedWord==word&&activeSet==1:
+			Owned.text+="[pull]"
+			Owned.text+=Word.storedWords[word]
+			Owned.text+="[/pull] "
+			continue
+		Owned.text+=Word.storedWords[word]+" "
+
+
+#inserts word into current phrase
+func insertWord(_word):
+	var _splitWords=BaseText.split(" ")
+	_splitWords.insert(_splitWords.size()-1,_word)
+	BaseText=buildPhrase(_splitWords,false)
+	buildPhrase(_splitWords)
+	
+	
