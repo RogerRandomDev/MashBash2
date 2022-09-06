677eb0b4b943cedfd4ae1b06bd42cbafa06c8255
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-09-06 15:22:00 -0700

    working on transitions now

---
+++ AMM 2 idea testing/addons/transitionScene/transitions.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+
+
+func transitionScene(target):
+	pass
--- AMM 2 idea testing/entities/items/swapCharger/swapCharger.gd
+++ AMM 2 idea testing/entities/items/swapCharger/swapCharger.gd
@@ -3,9 +3,13 @@ extends Position2D
 class_name WordSwapCharger
 var check=PhysicsShapeQueryParameters2D.new()
 var sprite=Sprite2D.new()
+var particles=GPUParticles2D.new()
 @export var chargeAmount:int=1
 func _ready():
-	add_child(sprite)
+	add_child(sprite);sprite.add_child(particles)
+	particles.process_material=preload("res://entities/shaderBased/pickupAuraparticles.tres")
+	particles.trail_enabled=true;particles.trail_length_secs=0.25;particles.trail_section_subdivisions=1;particles.trail_sections=3
+	particles.amount=12
 	sprite.texture=load("res://entities/flyingBot/botbody.png")
 	check.shape=RectangleShape2D.new();check.shape.extents=Vector2(4,4)
 	check.transform=Transform2D(rotation,position)
