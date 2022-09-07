301b9466d22995f94b5b41c4613c85fefb6c02b7
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-09-07 15:22:58 -0700

    adding the transition scene

--- AMM 2 idea testing/addons/transitionScene/transitions.gd
+++ AMM 2 idea testing/addons/transitionScene/transitions.gd
@@ -1,5 +1,14 @@
 extends CanvasLayer
-
+var toScene=""
 
 func transitionScene(target):
-	pass
+	toScene=target
+	get_tree().paused=true;Pausemenu.canPause=false
+
+
+
+func finishLoadScene():
+	get_tree().change_scene(toScene)
+
+func playScene():
+	get_tree().paused=false;Pausemenu.canPause=true
